4aef5e218cf879b8dab7777dd5a88fada54b82c2
dev231626 <nebiyudawit820@gmail.com>
2025-02-24 20:11:01 +0000

    new

--- evangadi_full_stack/Phase 3: Node, Express, MySql and React.js/script/script.sh
+++ evangadi_full_stack/Phase 3: Node, Express, MySql and React.js/script/script.sh
@@ -5,7 +5,25 @@
 pwd
 
 
-
+# to create and delete a branch 
+# git checkout -b branch_name = creates a new branch and switches to it
+# git branch = lists all the branches
+# git checkout branch_name = switches to the branch
+# git branch -d branch_name = deletes the branch
+# git push origin branch_name = pushes the branch to the remote repository
+# git push origin --delete branch_name = deletes the branch from the remote repository
+# git branch -m old_branch_name new_branch_name = renames the branch
+# git branch -a = lists all the branches including the remote branches
+# git branch -r = lists all the remote branches
+# git branch -vv = lists all the branches with the last commit message
+# git branch -vv = lists all the branches with the last commit message
+# git branch -u origin/branch_name = sets the upstream branch
+# git branch --unset-upstream = removes the upstream branch
+# git branch --set-upstream-to=origin/branch_name = sets the upstream branch
+# git branch --set-upstream-to=origin/branch_name = sets the upstream branch
+# git branch --set-upstream-to=origin/branch_name = sets the upstream branch
+# git branch --set-upstream-to=origin/branch_name = sets the upstream branch
+# git branch --set-upstream-to=origin/branch_name = sets the upstream branch
 
 
 
