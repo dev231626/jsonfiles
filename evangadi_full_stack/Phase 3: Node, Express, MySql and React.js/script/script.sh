9238ba5a0d879ea534d1d5520d1d08f169eafa77
dev neba <148289328+dev231626@users.noreply.github.com>
2025-02-26 07:29:18 +0000

    Pending changes exported from your codespace

--- evangadi_full_stack/Phase 3: Node, Express, MySql and React.js/script/script.sh
+++ evangadi_full_stack/Phase 3: Node, Express, MySql and React.js/script/script.sh
@@ -4,16 +4,16 @@
 
 pwd
 
-
+- pull - requests are used to ask someone to review the code that have been updated/edited and merge it to the main branch if it acceptable or reject it if it is not acceptable
 # to create and delete a branch 
-# git checkout -b branch_name = creates a new branch and switches to it
+# git checkout -b branch_name = creates a new branch and switches to it / - git branch branch_name = creates a new branch
 # git branch = lists all the branches
 # git checkout branch_name = switches to the branch
 # git branch -d branch_name = deletes the branch
 # git push origin branch_name = pushes the branch to the remote repository
 # git push origin --delete branch_name = deletes the branch from the remote repository
 # git branch -m old_branch_name new_branch_name = renames the branch
-# git branch -a = lists all the branches including the remote branches
+# # git branch -a = lists all the branches including the remote branches
 # git branch -r = lists all the remote branches
 # git branch -vv = lists all the branches with the last commit message
 # git branch -vv = lists all the branches with the last commit message
@@ -28,6 +28,10 @@ pwd
 
 
 
+-1 for execute
+-2 for write
+-4 for read
+-3 for write and execute 
 
 
 
